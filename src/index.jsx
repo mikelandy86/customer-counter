9287f0635b4642442718d4dbe4f4ae3f18e20838
mikelandy <mike.landy@gmail.com>
2022-12-19 20:08:33 +0100

    Fixed link path_Test15

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let countEl = document.getElementById("count-el");
+let count = 0;
+let saveEl = document.getElementById("save-el"); // Grabbing the element of save text (Won't work because React Project?)
+let saveAndSep = "";
+
+function increment() {
+  count += 1;
+  document.getElementById("count-el").innerText = count;
+}
+
+function save() {
+  saveAndSep = count + " - ";
+  document.getElementById("save-el").innerHTML += saveAndSep;
+  count = 0;
+  document.getElementById("count-el").innerText = count;
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  // 3. Render the variable in the saveEl using innerText
+  // NB: Make sure to not delete the existing content of the paragraph
+}
