739cf508fb8148e06919bdeb22fd372948716bb3
mikelandy <mike.landy@gmail.com>
2022-12-19 20:24:30 +0100

    Fixed link path_Test19

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import "./index.jsx";
+// import "./index.jsx";
 function App() {
   return (
     <div className="App">
       <div className="app-container">
         <h1>Customer counter!</h1>
-        <h2 id="count-el">{count}</h2>
-        <button id="increment-btn" onClick={increment}>
-          Increment
-        </button>
-        <button id="save-btn" onClick={save}>
-          Save
-        </button>
+        <h2 id="count-el">2</h2>
+        <button id="increment-btn">Increment</button>
+        <button id="save-btn">Save</button>
         <p id="save-el">Previous numbers: {"\u00A0"}</p>
       </div>
     </div>
